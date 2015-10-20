6836cb50eb7ca283835feca6470c74b4040b3408
River Merrill <rivermerrill.me@gmail.com>
2015-10-20 14:09:09 -0600

    v1.0.3 - UI update

--- public/jquery.js
+++ public/jquery.js
@@ -1,18 +1,35 @@
+// $('.button').hover(function () {
+// 	$(this).velocity({ 'width': '200px', 'height': '200px' }, 150)
+// }, function () {
+// 	$(this).velocity({ 'width': '150px', 'height': '150px' }, 150)
+// })
 $('.button').hover(function(){
-	$(this).velocity({'width': '200px', 'height': '200px'}, 150)
+	$(this).velocity({'border-radius': '30%'}, 150)
 }, function(){
-	$(this).velocity({'width': '150px', 'height': '150px'}, 150)
+	$(this).velocity({'border-radius': '50%'}, 150)
 })
-$('.button').on('click', function(){
-	$(this).css('background-color', 'lightgreen');
-})
-$('.reset').on('click', function(){
+
+
+
+$('.reset').on('click', function () {
+	$('.sonic').css('left', '46%');
+	$('.sonic').css('visibility', 'hidden');
 	$('.button').css('background-color', '');
+	$('.button').velocity({ 'left': '0%' })
+	$('.phoneList').animate({ 'height': '0%' }, 500);
+	$('body').css('background-color', '#1d2023');
 })
-$('.find').on('click', function(){
-	$('.phoneList').animate({'height': '100%'}, 1000);
-	$('body').css('background-color', 'white')
+$('.button').on('click', function () {
+	$('.button').velocity({ 'left': '-100%' });
+	$('.sonic').css('visibility', 'visible');
+	$('.sonic').velocity({ 'left': '0%' });
+	$('.sonic').delay(7000).velocity({ 'left': '-100%' });
+	$('.phoneList').delay(8000).animate({ 'height': '100%' }, 1000);
+	setTimeout(function () {
+		$('body').css('background-color', 'white')
+	}, 8000);
+	setTimeout(function(){
 	$('html, body').animate({
         scrollTop: $(".phoneList").offset().top
-    }, 2000);
+    }, 2000)}, 8000);
 })
--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@
 var app = angular.module('PhoneMines', []);
 
 app.controller('MainController', MainController);
-function MainController($http) {
+function MainController($http, $timeout) {
 	var vm = this;
 	vm.phones = [];
 	vm.finalPhones = [];
@@ -14,28 +14,68 @@ function MainController($http) {
 		this.features = features;
 	}
 	this.request = function (keyword) {
-		vm.keywords += ',' + keyword;
-		$http.post('http://localhost:1337/search.html', {
-			'category': 'Electronics',
-			'keywords': vm.keywords
-		}).then(function (data) {
-			for (var i = 0; i <= 5; i++) {
-				var attributes = data.data.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0];
-				var phone = new Phone(attributes.Title[0], attributes.Manufacturer[0], data.data.ItemSearchResponse.Items[0].Item[i].ImageSets[0].ImageSet[0].LargeImage[0].URL[0], attributes.Feature.join('.  '))
-				vm.phones.push(phone);
-			}
-		}, function (err) {
-			console.log('Cannot post.  Error:' + err);
-		});
+		$timeout(function () {
+			vm.keywords += ',' + keyword;
+			$http.post('http://localhost:1337/search.html', {
+				'category': 'Electronics',
+				'keywords': vm.keywords
+			}).then(function (data) {
+				for (var i = 0; i <= 5; i++) {
+					var attributes = data.data.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0];
+					var phone = new Phone(attributes.Title[0], attributes.Manufacturer[0], data.data.ItemSearchResponse.Items[0].Item[i].ImageSets[0].ImageSet[0].LargeImage[0].URL[0], attributes.Feature.join('.  '))
+					vm.phones.push(phone)
+				};
+				$timeout(function () {
+					vm.phones.forEach(function (phone) {
+						vm.finalPhones.push(phone)
+					});
+				}, 5000);
+			}, function (err) {
+				console.log('Cannot post.  Error:' + err);
+			});
+		}, 3000);
 	};
 	this.sendRequest = function () {
-		vm.phones.forEach(function (phone) {
-			vm.finalPhones.push(phone)
-		});
+		$timeout(function () {
+			vm.phones.forEach(function (phone) {
+				vm.finalPhones.push(phone)
+			});
+		}, 5000);
 	}
-	this.reset = function(){
+	this.reset = function () {
 		vm.phones = [];
 		vm.finalPhones = [];
 		vm.keywords = '';
 	}
 }
+var square = new Sonic({
+
+	width: 100,
+	height: 50,
+	padding: 10,
+
+	stepsPerFrame: 2,
+	trailLength: 1,
+	pointDistance: .03,
+
+	strokeColor: '#FF7B24',
+
+	step: 'fader',
+
+	multiplier: 2,
+
+	setup: function () {
+		this._.lineWidth = 5;
+	},
+
+	path: [
+
+		['arc', 10, 10, 10, -270, -90],
+		['bezier', 10, 0, 40, 20, 20, 0, 30, 20],
+		['arc', 40, 10, 10, 90, -90],
+		['bezier', 40, 0, 10, 20, 30, 0, 20, 20]
+	]
+});
+
+square.play();
+$('.select').append(square.canvas);
